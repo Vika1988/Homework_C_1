5ea9c4431abf40405eda29fdc5c0fb7aa220c1e3
*V* <cnevzorov@yandex.ru>
2022-10-06 18:28:21 +0300

    Программа для определения большего числа из двух

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,3 +1,14 @@
 ﻿// Напишите программу, которая на вход принмает два числа и выдает, какое число больше, а какое меньше.
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+if(a>b)
+{
+    Console.WriteLine($"Число {a} больше числа {b} ");
+} 
+else
+{
+    Console.WriteLine($"Число {b} больше числа {a} ");
+}
