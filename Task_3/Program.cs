0bb1507c8ba5a9d81b7e5d831f7099760cec4d81
*V* <cnevzorov@yandex.ru>
2022-10-06 19:30:54 +0300

    Программа для определения Четности

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдает является ли число четным
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+if((a % 2) == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else{
+    Console.WriteLine("Число не четное");
+}
