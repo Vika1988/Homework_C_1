9e1e10f85059da59b61e8a0df7b3e360a6d105b2
*V* <cnevzorov@yandex.ru>
2022-10-06 19:07:20 +0300

    Программа для определения максимального из трех чисел

---
+++ Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход 3 числа и выдает максимальное их этих чисел
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+int max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+Console.WriteLine($"Максимальное из этих чисел = {max} ");
